c6e4bff98323c5ad944c0a0ada68630f21030eb1
ilham praditya <pradityailham04@gmail.com>
2023-10-10 01:26:43 +0700

    modified:   src/components/Trailer.jsx

--- src/components/Trailer.jsx
+++ src/components/Trailer.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import NavbarComponents from "./NavbarComponents";
-import { Footer } from "./footer";
+import { Footer } from "./Footer";
 import "../style/style.css";
 
 function Trailer() {
